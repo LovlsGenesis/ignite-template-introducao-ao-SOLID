71614b184bdf5fd5377a7864c726b225b4cfb040
Guillaume Zeldine <g.zeldine@hotmail.fr>
2021-08-22 22:24:12 -0300

    Done coding and tests 10/10

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if (!foundUser?.admin) {
+      throw new Error("User is not an admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
